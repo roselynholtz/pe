e380a9b2f6d47b25df6ab22939c394ae6e2b7e71
roselynholtz <130674415+roselynholtz@users.noreply.github.com>
2024-02-17 17:42:43 -0600

    nt headers, cleanup

--- include/pe.h
+++ include/pe.h
@@ -31,6 +31,9 @@ namespace pe
 		// dos header.
 		IMAGE_DOS_HEADER* m_dos_header;
 
+		// nt headers.
+		IMAGE_NT_HEADERS* m_nt_headers;
+
 		// are we a pe file?
 		bool m_pe;
 
@@ -49,6 +52,9 @@ namespace pe
 			// we aren't a pe file, don't bother.
 			if ( !m_pe )
 				return;
+
+			// IMAGE_DOS_HEADER::e_lfanew contains the offset of nt headers from our image base.
+			m_nt_headers = reinterpret_cast< IMAGE_NT_HEADERS* >( m_image_address + m_dos_header->e_lfanew );
 		}
 	public:
 		file( const std::vector< uint8_t >& image_data )
@@ -61,8 +67,8 @@ namespace pe
 			// populate our headers.
 			populate_headers( );
 
-			// we aren't a pe file, don't bother.
-			if ( !m_pe )
+			// initial setup failed, don't bother.
+			if ( !good( ) )
 				return;
 		}
 
@@ -88,19 +94,43 @@ namespace pe
 			m_image_address = 0x0;
 		}
 
-		// true if 'm_image_data' is not empty and this is a pe file.
+		// true if the file seems to have no issues.
+		// this is not extensive, compare and check data yourself.
 		bool good( )
 		{
-			return !m_image_data.empty( ) && m_pe;
+			// bad image data.
+			if ( m_image_data.empty( ) )
+				return false;
+
+			// not in pe format.
+			if ( !m_pe )
+				return false;
+
+			// invalid nt headers.
+			if ( !m_nt_headers )
+				return false;
+
+			return true;
 		}
 
+		// dos header for this file.
+		const IMAGE_DOS_HEADER* dos_header( ) const
+		{
+			return m_dos_header;
+		}
+
+		// nt headers for this image.
+		const IMAGE_NT_HEADERS* nt_headers( ) const
+		{
+			return m_nt_headers;
+		}
 	#ifdef PE_VERBOSE
 		// spews all debug data about the file.
 		void spew( )
 		{
 			std::cout << "----------------------------------" << std::endl;
 			std::cout << "|            pe file             |" << std::endl;
-			std::cout << "----------------------------------" << std::endl << std::endl;
+			std::cout << "----------------------------------" << std::endl;
 
 			// we don't have a valid file.
 			if ( !good( ) )
@@ -109,18 +139,11 @@ namespace pe
 				return;
 			}
 
-			std::cout << "file loaded" << std::endl;
-
-			if ( !m_pe )
-			{
-				std::cout << "file is not in pe format" << std::endl;
-				return;
-			}
-
-			std::cout << "file is in pe format" << std::endl;
+			std::cout << "note: most, if not all addresses stored here are relative to the base of the image" << std::endl << std::endl;
 
 			// headers.
 			// we skip dos header here since it is not useful.
+			std::cout << "nt headers: 0x" << std::hex << reinterpret_cast< uintptr_t >( m_nt_headers ) - m_image_address << std::endl;
 		}
 	#endif
 	};
